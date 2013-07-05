45b7e511caf72395cd3758ac171f9b32d517b6db
Kevin Attfield <k.attfield@gmail.com>
2013-07-05 12:35:41 -0700

    voice

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var app = express();
 var port = process.env.PORT || 5000;
 var twilio = require('twilio');
 var twilioClient = twilio('AC5176f9a3c768040fd0870342bbb2631e', '91fbafa8b064b3cd3bb9f3778d1b948b');
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/mydb'; 
+
+/*
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+*/
 
 app.use(express.logger());
 app.use(express.bodyParser());
@@ -11,37 +24,42 @@ app.get('/', function(request, response) {
     response.send('Why are you looking here?');
 });
 
+// When Twilio recieves an SMS it hits this route and listens for a TwiML response
+// http://www.twilio.com/docs/api/twiml
 app.post('/sms', function(req, res) {
-    var challenge = new twilio.TwimlResponse();
-    challenge.sms('Body:' + req.body.Body + ' From:' + req.body.From + ' SmsSid:' + req.body.SmsSid);
+    var text = req.body.Body;
+    var from = req.body.From;
+    var smssid= req.body.SmsSid;
+    var call = new twilio.TwimlResponse();
+
+    call.say('Please hold for the Angrologist', {
+        voice:'woman',
+        language:'en-gb'
+    })
+    .pause({ length: 3 })
+    .play('https://s3-us-west-2.amazonaws.com/angrologist/7-5-2013/horoscope.mp3');
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(challenge.toString());
+    res.end(call.toString());
 });
 
-app.get('/horoscope/:number', function(req, res) {
-    var target = req.params.number;
+// When Twilio recieves an phone call it hits this route and listens for a TwiML response
+// http://www.twilio.com/docs/api/twiml
+app.post('/voice', function(req, res) {
+    var text = req.body.Body;
+    var from = req.body.From;
+    var smssid= req.body.SmsSid;
+    var call = new twilio.TwimlResponse();
 
-    if(!target) res.send('No target');
+    call.say('Please hold for the Angrologist', {
+        voice:'woman',
+        language:'en-gb'
+    })
+    .pause({ length: 3 })
+    .play('https://s3-us-west-2.amazonaws.com/angrologist/7-5-2013/horoscope.mp3');
 
-    //Send an SMS text message
-    twilio.sendSms({
-
-        to: target, // Any number Twilio can deliver to
-        from: '+16475578011', // A number you bought from Twilio and can use for outbound communication
-        body: 'word to your mother.' // body of the SMS message
-
-    }, function(err, responseData) { //this function is executed when a response is received from Twilio
-
-        if (!err) { // "err" is an error received during the request, if any
-            // "responseData" is a JavaScript object containing data received from Twilio.
-            // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-            // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
-            console.log('Sent text to ' + responseData.to); // outputs "Seding text to +14506667788"
-            res.jsonp({target: target});
-        }
-    });
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(call.toString());
 });
 
 app.listen(port, function() {
